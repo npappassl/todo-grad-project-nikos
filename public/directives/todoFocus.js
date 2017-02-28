98a8e0e24153e37f768f2971edb85611cf18c691
Nikolaos Pappas <npappas@scottlogic.co.uk>
2017-02-28 16:57:33 +0000

    style checks

--- public/directives/todoFocus.js
+++ public/directives/todoFocus.js
@@ -4,10 +4,9 @@
  * Directive that places focus on the element it is applied to when the
  * expression it binds to evaluates to true
  */
-angular.module('todomvc')
-	.directive('todoFocus', function todoFocus($timeout) {
-		'use strict';
-
+angular.module("todomvc")
+	.directive("todoFocus", function todoFocus($timeout) {
+		"use strict";
 		return function (scope, elem, attrs) {
 			scope.$watch(attrs.todoFocus, function (newVal) {
 				if (newVal) {
@@ -17,4 +16,4 @@ angular.module('todomvc')
 				}
 			});
 		};
-	});
+});
